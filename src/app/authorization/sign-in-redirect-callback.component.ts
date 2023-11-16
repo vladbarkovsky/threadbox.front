b14eab18f2f650954c3d66d32b2fdd1a38f7567b
Vladislav Barkovsky <vladislav.barkovsky@bbit.tech>
2023-11-16 21:40:15 +0200

    (҂◡_◡)

--- src/app/authorization/sign-in-redirect-callback.component.ts
+++ src/app/authorization/sign-in-redirect-callback.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from './authorization.service';
 
 @Component({
-  template: '',
+  template: 'app-sign-in-redirect-callback',
 })
 export class SignInRedirectCallbackComponent implements OnInit {
   constructor(private authorizationService: AuthorizationService) {}
